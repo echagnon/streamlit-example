c17ce72907863f5bd56f6bce629d573e900158a0
echagnon <38565726+echagnon@users.noreply.github.com>
2023-11-24 18:39:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,7 +77,7 @@ prompt = st.text_input('Write your prompt!')
 st.write("This is your prompt", prompt)
 
 if "Éric" in prompt or "eric" in prompt :
-    prompt_response = "Éric est génial, il adore Catherine!!!")
+    prompt_response = "Éric est génial, il adore Catherine!!!"
 elif len(prompt) > 0 and gpt_choice == ":rainbow[GPT3.5]" :
     prompt_response = prompt_open_ai(prompt, 35)
 elif len(prompt) > 0 and gpt_choice == "***GPT4.0***" :
